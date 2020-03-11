4f785e76f7b0146672bdeb5d8c12b425b918d16f
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-11 19:42:21 +1100

    server app

--- server/app/index.ts
+++ server/app/index.ts
@@ -1,5 +1,4 @@
 import express, { Application } from 'express';
-import path from 'path';
 import cors from 'cors';
 import morgan from 'morgan';
 
@@ -8,7 +7,6 @@ const app:Application = express();
 // set cors & logs
 app.use(cors());
 app.use(morgan('dev'));
-
 
 // parsing application body
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +19,7 @@ if(process.env.NODE_ENV === 'production') {
 	// set static folder
 	// app.use(express.static(path.resolve(__dirname, '..', '..', 'client', 'build')));
 	//app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html')));
+	console.log('prod')
 }
 
 export default app;
