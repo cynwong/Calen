6e80bede53a3092052b88794deecae0e5afda2f8
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-14 10:13:01 +1100

    add home and Signup route to client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './Components/Pages/Home';
+import SignUp from './Components/Pages/SignUp';
+
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="wrapper">
+			<Router>
+				
+				<main>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/signup' component={SignUp} />
+				</main>
+			</Router>
 		</div>
 	);
 }
