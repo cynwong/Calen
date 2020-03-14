30c39c395d08e01e942cd35d61db2a0084b25373
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-14 20:39:36 +1100

    Add Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Header from './Components/NavBar/Header';
 import Home from './Components/Pages/Home';
 import SignUp from './Components/Pages/SignUp';
 
@@ -10,7 +11,7 @@ function App() {
 	return (
 		<div className="wrapper">
 			<Router>
-				
+				<Header />
 				<main>
 					<Route exact path='/' component={Home} />
 					<Route exact path='/signup' component={SignUp} />
