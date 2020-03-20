e9dd2aeceb8f8f60431b2e8f204355de28869bad
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-21 10:50:32 +1100

    Add logout route

--- server/app/routes/api/index.ts
+++ server/app/routes/api/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import signUpRoute from './signup';
 import loginRoute from './login';
+import logOutRoute from './logout';
 
 const router = Router();
 
 router.use('/signup', signUpRoute);
 router.use('/login', loginRoute);
+router.use('/logout', logOutRoute);
 
 export default router;
