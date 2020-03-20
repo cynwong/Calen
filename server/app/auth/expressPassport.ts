c8cf3bc56ac4cfdbbb448e77ff4634b7cbbd0559
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-21 09:36:58 +1100

    express middlewares for passport

---
+++ server/app/auth/expressPassport.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+
+// Authentication Middleware functions
+// Do not allow user to access without login 
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+}
+
+// do not allow login-user to access
+export const forwardIfNotAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+	if (!req.isAuthenticated()) {
+		return next();
+	}
+	return res.redirect('/login'); //TODO go to dashboard
+}
