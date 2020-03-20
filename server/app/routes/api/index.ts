3bb33d2c322608741e6246b1ec6529269dbc66b1
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-21 09:37:23 +1100

    Add login route

--- server/app/routes/api/index.ts
+++ server/app/routes/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import signUpRoute from './signup';
+import loginRoute from './login';
 
 const router = Router();
 
 router.use('/signup', signUpRoute);
+router.use('/login', loginRoute);
 
 export default router;
