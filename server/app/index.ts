9a8f94737ff4859c50e491090855cc0a05001f4d
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-21 10:01:13 +1100

    setup passport middleware

--- server/app/index.ts
+++ server/app/index.ts
@@ -1,19 +1,42 @@
-import express, { Application } from 'express';
 import cors from 'cors';
+import express, { Application } from 'express';
+import expressSession from 'express-session';
 import morgan from 'morgan';
+import passport from 'passport';
+
+import { secretKey } from './auth/config';
+import configurePassport from './auth/passport';
 
 // routes
 import apiRoutes from './routes/api';
 
 const app:Application = express();
 
-// set cors & logs
+// configure passport
+configurePassport(passport);
+
+// set cors & logs middlewares
 app.use(cors());
 app.use(morgan('dev'));
 
-// parsing application body
+// parsing application body middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Express Session middlewares
+app.use(expressSession({ 
+	secret: secretKey as string, 
+	resave: true, 
+	saveUninitialized: true,
+	cookie: {
+		maxAge: 604800000,
+		secure: true,
+	}
+}));
+
+// Passport middlewares
+app.use(passport.initialize());
+app.use(passport.session());
 
 // import routes
 app.use('/api', apiRoutes);
