c6b5a732706ffbaf581507cfe71d396dcad30e89
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-21 10:48:37 +1100

    remove console statement

--- server/app/routes/api/login/index.ts
+++ server/app/routes/api/login/index.ts
@@ -8,29 +8,24 @@ const loginRoute = Router();
 
 loginRoute.post(
 	'/',
-
 	(req:Request, res: Response, next: NextFunction): void => {
-		console.log(req.body);
-		console.log('=====')
 		passport.authenticate('local', (err:Error, user:UserDocument): void => {
-			console.log({user, err});
 			if (err) {
 				console.error(err);
 				return next(err);
 			}
 			if (!user) {
-				console.log("no user");
-				res.status(401).json({ errors: ['Incorrect username or password']});
+				res
+					.status(401)
+					.json({ errors: ['Incorrect username or password']});
 				return;
 			}
 			// log in user
 			req.logIn(user, (loginErr:Error): void => {
-				console.log(user)
 				if (loginErr) {
 					console.error(loginErr);
 					return next(loginErr);
 				}
-				console.log(user);
 				res.status(200).json({ success: true });
 			});
 		})(req,res,next);
