3c99a9bb55eaa3f814d515d099bf890cda14eb04
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-23 21:39:51 +1100

    Update Dashboard to use fullcalendar

--- client/src/Components/Pages/DashBoard/DashBoard.page.js
+++ client/src/Components/Pages/DashBoard/DashBoard.page.js
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
-import './styles.scss';
 
-import BigCalendar from './BigCalendar/BigCalendar';
+import FullCalendarComponent from './FullCalendar/FullCalendar';
+// import BigCalendar from './BigCalendar/BigCalendar';
+
+
+// utils
+import AppContext from '../../../utils/AppContext';
+
+// Styles
+import './DashBoard.styles.scss';
 
 export default function DashBoard() {
+	const { user: { events: userEvents }} = useContext(AppContext);
+
+	const [events, setEvents] = useState(userEvents); //TODO get from server
+
+	// const handleSelect = ({start, end}) => {
+	// 	console.log(start, end);
+	// 	// const title = window.prompt('New Event name')
+	// 	// if (title){
+	// 	// 	setEvents([
+	// 	// 		...events,
+	// 	// 		{
+	// 	// 			start,
+	// 	// 			end,
+	// 	// 			title,
+	// 	// 		},
+	// 	// 	]);
+	// 	// }
+	// };
+
+	// const handleEventClick = (info) => {
+	// 	console.log('event click', info)
+	// 	console.log(info);
+	// };
 
 	return (
 		<div className='calendar-container'>
-			<BigCalendar />
+			{/* <BigCalendar
+				events={events}
+				handleSelect={handleSelect}
+				handleEventClick={handleEventClick}
+			/> */}
+			<FullCalendarComponent events={events} />
 		</div>
 	)
 }
