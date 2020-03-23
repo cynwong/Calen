aa3ffd5db70e892194ec643e071aab638eb1c730
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-23 21:41:44 +1100

    Remove redirection from home

--- client/src/Components/Pages/Home/Home.page.js
+++ client/src/Components/Pages/Home/Home.page.js
@@ -1,16 +1,8 @@
-import React, {useContext} from 'react'
+import React from 'react'
 
 import './styles.scss';
 
-import AppContext from '../../../utils/AppContext';
-import { useHistory } from 'react-router-dom';
-
 export default function Home() {
-	const {user} = useContext(AppContext);
-	const history= useHistory();
-	if(user.username){
-		history.push('/dashboard');
-	}
 	return (
 		<div className='home'>
 			
