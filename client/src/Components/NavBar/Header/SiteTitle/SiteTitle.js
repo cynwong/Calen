b334589dd77ff41edd1831dc6a9e3e27e094d744
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-23 21:44:42 +1100

    rename site title filename

--- client/src/Components/NavBar/Header/SiteTitle/SiteTitle.js
+++ client/src/Components/NavBar/Header/SiteTitle/SiteTitle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import './styles.scss';
+import './SiteTitle.styles.scss';
 
 export default function SiteTitle() {
 	return (
