8be46d08eb8e75d4fcd3a3958c0ab30119d8655d
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-23 19:07:51 +1100

    Get all Events route

---
+++ server/app/routes/api/events/getAllEvents/index.ts
@@ -0,0 +1,24 @@
+import { Router, Request, Response } from 'express';
+
+import { checkIfAuthenticated } from '../../../../auth/expressPassport';
+import { getAllEvents } from '../../../../../database/controllers/EventController';
+
+const allEventsRoute:Router = Router();
+
+allEventsRoute.get(
+	'/',
+	checkIfAuthenticated,
+	async (req:Request, res: Response): Promise<void> => {
+		const { user } = req;
+		console.log(user);
+		try {
+			const events = await getAllEvents(user.id);
+			res.status(200).json(events);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({error: true});
+		}
+	}
+);
+
+export default allEventsRoute;
