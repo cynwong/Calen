4a6ccb9257295777cde2c69f90bed53158b633a1
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-23 11:33:12 +1100

    Add home redirect if login

--- client/src/Components/Pages/Home/index.js
+++ client/src/Components/Pages/Home/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import './styles.scss';
 
+import AppContext from '../../../utils/AppContext';
+import { useHistory } from 'react-router-dom';
+
 export default function Home() {
+	const {user} = useContext(AppContext);
+	console.log(user);
+	const history= useHistory();
+	if(user.username){
+		history.push('/dashboard');
+	}
 	return (
 		<div className='home'>
 			
