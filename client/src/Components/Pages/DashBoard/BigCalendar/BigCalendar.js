ed3052795f11f3e6daf34821abf5d1dd54276935
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-23 21:41:12 +1100

    Big Calendar

--- client/src/Components/Pages/DashBoard/BigCalendar/BigCalendar.js
+++ client/src/Components/Pages/DashBoard/BigCalendar/BigCalendar.js
@@ -1,50 +1,24 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from 'moment';
-import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
-
-// utils
-import AppContext from '../../../../utils/AppContext';
 
 // styles
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
+import './BigCalendar.styles.scss';
 
-const BigCalendar = () => {
-	const { user: { events: userEvents }} = useContext(AppContext);
+const BigCalendar = (props) => {
+	const {
+		events,
+		handleSelect,
+		handleEventClick
+	} = props;
 
 	const localizer = momentLocalizer(moment);
-	const DnDCalendar = withDragAndDrop(Calendar);
-	const [events, setEvents] = useState(userEvents); //TODO get from server
-
-	const handleSelect = ({ start, end }) => {
-		const title = window.prompt('New Event name')
-		if (title){
-			setEvents([
-				...events,
-				{
-					start,
-					end,
-					title,
-				},
-			]);
-		}
-	}
-	const handleEventClick = (info) => {
-		console.log('event click', info)
-		console.log(info);
-	}
-
-	const handleEventResize = (info) => {
-		console.log('event resize', info)
-	}
-	const handleEventDrop = (info) => {
-		console.log('event drop', info)
-	}
 
 	return (
-		<DnDCalendar
+		<Calendar
 			defaultDate={moment().toDate()}
 			defaultView="month"
 			views={Object.keys(Views).map(k => Views[k])}
@@ -53,15 +27,10 @@ const BigCalendar = () => {
 			step={15}
 			showMultiDayTimes={true}
 			scrollToTime={new Date()}
-			defaultDate={new Date()}
-			defaultView={Views.WEEK}
 			selectable
 			popup
-			onEventDrop={handleEventDrop}
-			onEventResize={handleEventResize}
 			onSelectEvent={handleEventClick}
 			onSelectSlot={handleSelect}
-			resizable
 			style={{ height: "100vh" }}
         />
 	);
