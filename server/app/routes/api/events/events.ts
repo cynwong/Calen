5c031bcfa9f875c50455d2c47ef71e0cb26a9f23
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-24 21:29:59 +1100

    Rename events route file name

--- server/app/routes/api/events/events.ts
+++ server/app/routes/api/events/events.ts
@@ -1,9 +1,11 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
 
 import allEventsRoute from './getAllEvents';
+import postNewEvent from './postNewEvent';
 
 const eventsRoutes = Router();
 
+eventsRoutes.post('/', postNewEvent);
 eventsRoutes.use('/all', allEventsRoute);
 
 export default eventsRoutes;
