a67dc96386e717742609f480fd2259b14fd6ad90
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-24 23:02:25 +1100

    postNewEvent route

--- server/app/routes/api/events/subRoutes/postNewEvent.ts
+++ server/app/routes/api/events/subRoutes/postNewEvent.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from 'express';
 
 import { checkIfAuthenticated } from '../../../../auth/expressPassport';
-// import { getAllEvents } from '../../../../../database/controllers/EventController';
+import { createEvent } from '../../../../../database/controllers/EventController';
+import { EventDocument } from '../../../../../database/models/Event';
 
 const postNewEvent:Router = Router();
 
@@ -10,15 +11,31 @@ postNewEvent.post(
 	checkIfAuthenticated,
 	async (req:Request, res: Response): Promise<void> => {
 		const { user, body } = req;
-		console.log({user, body});
-		res.end();
-		// try {
-		// 	const events = await getAllEvents(user.id);
-		// 	res.status(200).json(events);
-		// } catch (err) {
-		// 	console.error(err);
-			// res.status(500).json({error: true});
-		// }
+		const {
+			title,
+			start,
+			allDay,
+			end,
+			desc,
+			location,
+			notes
+		} = body;
+		try {
+			await createEvent({
+				creator: user?._id,
+				title,
+				start,
+				allDay,
+				end,
+				desc,
+				location,
+				notes
+			} as EventDocument);
+			res.status(200).json({success: true});
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({error: true});
+		}
 	}
 );
 
