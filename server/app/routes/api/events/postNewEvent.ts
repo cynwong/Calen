e54191bd75e6f45089d830b62da7bcb907962fd2
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-24 21:30:51 +1100

    rename and relocate post new event file

---
+++ server/app/routes/api/events/postNewEvent.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+
+// import { getAllEvents } from '../../../../../database/controllers/EventController';
+
+const postNewEvent:Router = Router();
+
+postNewEvent.post(
+	'/',
+	async (req:Request, res: Response): Promise<void> => {
+		const { user, body } = req;
+		console.log({user, body});
+		res.end();
+		// try {
+		// 	const events = await getAllEvents(user.id);
+		// 	res.status(200).json(events);
+		// } catch (err) {
+		// 	console.error(err);
+			// res.status(500).json({error: true});
+		// }
+	}
+);
+
+export default postNewEvent;
