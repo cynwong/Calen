4794e5bd4ef89c1587e8c7043d4ae02c80b8ae52
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-24 21:53:21 +1100

    Add mongo-connect

--- server/app/app.ts
+++ server/app/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express, { Application } from 'express';
 import expressSession from 'express-session';
 import morgan from 'morgan';
+import {default as connectMongo} from 'connect-mongo';
 
 import { secretKey } from './auth/config';
 import passport from './auth/passport';
@@ -20,14 +21,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Express Session middlewares
+const MongoStore = connectMongo(expressSession);
 app.use(expressSession({ 
 	secret: secretKey as string, 
-	resave: true, 
-	saveUninitialized: true,
-	// cookie: {
-	// 	maxAge: 604800000,
-	// 	secure: true,
-	// }
+	resave: false, 
+	saveUninitialized: false,
+	store: new MongoStore({
+		url: process.env.NODE_ENV === 'development' ? process.env.LOCAL_DB_CONNECTION_URI as string : process.env.MONGODB_URI as string,
+		autoRemove: 'interval',
+		autoRemoveInterval: 10080 // In minutes. Default
+	})
 }));
 
 // Passport middlewares
