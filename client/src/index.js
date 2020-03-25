d44fa4e8141fd44a2146e620a1c680e658f12337
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-25 21:19:47 +1100

    Add Fonts for site title

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,10 @@ import App from './App';
 import './index.scss';
 import './reset.scss';
 
+import './fonts/FredokaOne-Regular.ttf';
+import './fonts/Lobster-Regular.ttf';
+import './fonts/Playball-Regular.ttf';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
