76a7657440680b93bbc2ab838406c9de8b3498f0
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-26 18:13:14 +1100

    Add check if user is owner

--- server/app/routes/api/events/subRoutes/getEvent.ts
+++ server/app/routes/api/events/subRoutes/getEvent.ts
@@ -6,14 +6,18 @@ import { UserDocument } from 'server/database/models/User';
 const allEventsRoute:Router = Router();
 
 allEventsRoute.get(
-	'/',
+	'/:id',
 	async (req:Request, res: Response): Promise<void> => {
 		const { user } = req;
 		const { id } = req.params;
 		const userId = user && (user as UserDocument)._id;
 		try {
 			const event = await getEventById(id);
-			if( event.creatorId !==  userId ) {
+			if( !event.creatorId ){
+				throw new Error;
+			}
+			// check if user is the creator of this event. Otherwise don't give the data
+			if( String(event.creatorId) !==  String(userId) ) {
 				res.status(401).json({error: 'unauthorized'});
 				return;
 			}
