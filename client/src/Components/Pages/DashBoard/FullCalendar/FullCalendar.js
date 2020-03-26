3997c159a7cec153fdb778944dfb60ed655075dc
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-26 18:15:44 +1100

    Use history hook for new event

--- client/src/Components/Pages/DashBoard/FullCalendar/FullCalendar.js
+++ client/src/Components/Pages/DashBoard/FullCalendar/FullCalendar.js
@@ -1,5 +1,8 @@
 
-import React, { createRef, useState, useContext } from 'react';
+import React, { createRef, useContext } from 'react';
+
+import { useHistory } from 'react-router-dom';
+
 import moment from 'moment';
 
 import FullCalendar from '@fullcalendar/react';
@@ -8,71 +11,52 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
 
-import TransitionsModal from '../../../common/TransitionModal/TransitionModal';
-
 import AppContext from '../../../../utils/AppContext';
 
 import './FullCalendar.styles.scss';
 
 export default function FullCalendarComponent() {
 	const { user: { events }} = useContext(AppContext);
-	const [modalData, setModalData] = useState({
-		open: false,
-		event: {}
-	});
 	const calendarComponentRef = createRef();
 
 	const dateFormatString = "YYYY-MM-DDThh:mm";
+	const history = useHistory();
 
 	const calendarSettings = {
 		calendarWeekends: true,
 		droppable: true,
 		editable: true,
 	}
-	const selectDates = (info) => {
-		const { allDay, endStr, startStr} = info;
-		setModalData({
-			open: true,
-			event: {
-				allDay,
-				start: moment(startStr).format(dateFormatString),
-				end: moment(endStr).format(dateFormatString),
-			}
-		});
+	const selectDates = ({ allDay, endStr, startStr}) => {
+		const start = moment(startStr).format(dateFormatString);
+		const end = moment(endStr).format(dateFormatString);
+		history.push(`/events/new?start=${start}&end=${end}&allDay=${allDay}`);
 	};
 
-	const closeModal = () => setModalData({open: false, event: {}});
-	const clickDate = (info) => {
-		console.log('date clicked>>>>')
-		console.log(info)
-	};
+	const eventClick = ({event}) => {
+		const {id} = event;
+		history.push(`events/${id}`);
+	}
 	
 	return (
 		<div className='full-calendar-container'>
-			{
-				modalData.open ? (
-					<TransitionsModal open={modalData.open} event={modalData.event} closeModal={closeModal} />
-				) : (
-					<FullCalendar 
-						header={{
-							left: 'today',
-							center: 'prev,title,next',
-							right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-						}}
-						plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
-						ref={ calendarComponentRef }
-						weekends={ calendarSettings.calendarWeekends }
-						events={ events }
-						navLinks={true}
-						eventLimit={true}
-						selectable={true}
-						editable={true}
-						select= {selectDates}
-						clickDate={clickDate}
-					/>
-				)
-			}
-			
+			<FullCalendar 
+				header={{
+					left: 'today',
+					center: 'prev,title,next',
+					right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+				}}
+				plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
+				ref={ calendarComponentRef }
+				weekends={ calendarSettings.calendarWeekends }
+				events={ events }
+				navLinks={true}
+				eventLimit={true}
+				selectable={true}
+				// editable={true}
+				select= {selectDates}
+				eventClick={eventClick}
+			/>
 		</div>
-	)
+	);
 }
