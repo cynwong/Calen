4c1ca905d18e5e9ddc98a5dcb431bc572d66fe18
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-26 22:51:55 +1100

    Put event route

---
+++ server/app/routes/api/events/subRoutes/putEvent.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response } from 'express';
+
+import { getEventById } from '../../../../../database/controllers/EventController';
+import { UserDocument } from 'server/database/models/User';
+
+const putEventRoute:Router = Router();
+
+putEventRoute.put(
+	'/:id',
+	async (req:Request, res: Response): Promise<void> => {
+		const { user, body } = req;
+		const { id } = req.params;
+		const userId = user && (user as UserDocument)._id;
+
+		try {
+			const event = await getEventById(id);
+
+			if( !event.creatorId ) {
+				throw new Error;
+			}
+			// check if user is the creator of this event. Otherwise don't give the data
+			if( String(event.creatorId) !==  String(userId) ) {
+				res.status(401).json({error: 'unauthorized'});
+				return;
+			}
+			event.title = body.title;
+			event.start = body.start;
+			event.end = body.end;
+			event.allDay = body.allDay;
+			event.desc = body.desc;
+			event.location = body.location;
+			event.notes = body.notes;
+			await event.save();
+			res.status(200).json(event);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({error: true});
+		}
+	}
+);
+export default putEventRoute;
