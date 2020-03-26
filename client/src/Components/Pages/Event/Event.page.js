41446bdb5ed388bc4dacaaf84d3f963e8400b934
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-27 00:14:13 +1100

    Remove unused vars

--- client/src/Components/Pages/Event/Event.page.js
+++ client/src/Components/Pages/Event/Event.page.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState }  from 'react';
+import React, { useContext }  from 'react';
 
 import { useParams } from 'react-router-dom';
 
@@ -8,17 +8,14 @@ import {
 	Paper,
 	Typography
 } from '@material-ui/core';
-import Alert from '@material-ui/lab/Alert';
 
 import EventForm from '../../common/EventForm/EventForm';
 
 import AppContext from '../../../utils/AppContext';
-import API from '../../../utils/API';
 
 export default function Event() {
 	const { classes, user: { events } } = useContext(AppContext);
 	const { id } = useParams();
-	const [severError, setSeverError] = useState(false);
 	const event = events.filter((e)=> e.id === id)[0];
 
 	return (
@@ -27,7 +24,6 @@ export default function Event() {
 				<Typography variant="h2" className={classes.formTitle} gutterBottom>
 					Edit event
 				</Typography>
-				{ severError && <Alert severity="error">Something went wrong with loading data. Try again later.</Alert> }
 				<EventForm event={event}/>
 			</Paper>
 		</Container>
