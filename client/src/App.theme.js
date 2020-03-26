76f5d86aa570db9026509fb7617de2643c6857ab
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-26 11:00:00 +1100

    combine import declations

--- client/src/App.theme.js
+++ client/src/App.theme.js
@@ -1,6 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { teal } from '@material-ui/core/colors';
-import pink from '@material-ui/core/colors/pink';
+import { teal, pink } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
 	typography: {
