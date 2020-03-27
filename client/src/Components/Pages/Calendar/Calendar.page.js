84e071b5e25056ec35b6990ce7050dc3ba7a12fb
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-28 10:29:10 +1100

    Update Calendar page

--- client/src/Components/Pages/Calendar/Calendar.page.js
+++ client/src/Components/Pages/Calendar/Calendar.page.js
@@ -8,24 +8,39 @@ import FullCalendarComponent from '../../common/FullCalendar/FullCalendar';
 
 import AppContext from '../../../utils/AppContext'
 
-export default function DashBoard() {
+export default function CalendarPage() {
 	const { classes, user: { events } } = useContext(AppContext);
 
 	const history = useHistory();
 
 	const selectDates = ({ allDay, endStr, startStr}) => {
-		history.push(`/events/new?start=${startStr}&end=${endStr}&allDay=${allDay}`);
+		history.push(`/calendar/new?start=${startStr}&end=${endStr}&allDay=${allDay}`);
 	};
 
 	const eventClick = ({event}) => {
 		const {id} = event;
-		history.push(`events/${id}`);
+		history.push(`calendar/${id}`);
 	};
+
+	const goBack = () => history.push('/calendar');
+
+	const header =  {
+		left: 'today',
+		center: 'prev,title,next',
+		right: 'dayGridMonth,timeGridWeek,timeGridDay,listDay'
+	},
 
 	return (
 		<Container className={classes.container}>
 			<Paper className={classes.bigPaper}>
-				<FullCalendarComponent events={events} selectDates={selectDates} eventClick={eventClick} leftHeader='today' rightHeader='dayGridMonth,timeGridWeek,timeGridDay,listDay' />
+				<FullCalendarComponent 
+					events={[...events].filter((e) => e.type === 0)}
+					header={header}
+					view='dayGridMonth'
+					selectDates={selectDates}
+					eventClick={eventClick} 
+					goBack={goBack}
+				/>
 			</Paper>
 		</Container>
 	)
