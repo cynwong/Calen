28e0d11f8f801a5634d5cb1fbf19df09b7e492a7
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-27 23:54:03 +1100

    Add date header text

--- client/src/Components/common/DiaryForm/DiaryForm.js
+++ client/src/Components/common/DiaryForm/DiaryForm.js
@@ -1,19 +1,22 @@
 import React, { useContext, useState } from 'react';
-import moment from 'moment';
-
 import { useHistory } from 'react-router-dom';
 
-import { TextField, Button } from '@material-ui/core';
+import moment from 'moment';
 
+import { Button, TextField, Typography } from '@material-ui/core';
+
+// text editor
 import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 
+// local component
 import AlertComponent from '../AlertComponent/AlertComponent';
 
 import AppContext from '../../../utils/AppContext';
 
+// styles
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './diaryForm.styles.scss'
 
@@ -30,15 +33,17 @@ export default function DiaryForm({event}) {
 	const history = useHistory();
 	const fnClose = () => history.push('/diary');
 	
-
-	const contentBlock = htmlToDraft(event.entry ? event.entry : '');
+	// set up editorState
+	const html = event.entry ? event.entry: '';
+	const contentBlock = htmlToDraft(html);
 	const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
 	const initialEditorState = EditorState.createWithContent(contentState);
 	const [editorState,setEditorState] = useState(initialEditorState);
 
 	const handleSaveBtnClick = async (e) => {
 		e.preventDefault();
-		const entry = draftToHtml(convertToRaw(initialEditorState.getCurrentContent()));
+		// convert to html for saving
+		const entry = draftToHtml(convertToRaw(editorState.getCurrentContent()));
 		try {
 			const updatingEvent = {
 				...event,
@@ -66,63 +71,68 @@ export default function DiaryForm({event}) {
 	}
 
 	return (
-		<form className={classes.form} noValidate>
-			{
-				serverError && 
-					<AlertComponent type='error' identifier='server' text='' />
-			}
-			<TextField
-				label="Date"
-				type="date"
-				defaultValue={moment(event.start).format('YYYY-MM-DD')}
-				className={classes.inputTextField}
-				InputProps={{
-					readOnly: true,
-					className: classes.input
-				}}
-				InputLabelProps={{
-					shrink: true,
-				}}
-			/>
-			
-			<Editor
-				toolbar={{options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'emoji',]	}}
-				editorState={editorState}
-				initialContentState={{}}
-				toolbarClassName="editor-toolbar"
-				wrapperClassName="editor-wrapper"
-				editorClassName="editor"
-				onEditorStateChange={(newState) => setEditorState(newState)}
-			/>
-			<footer className={classes.formFooter}>
+		<>
+			<Typography variant="h2" className={classes.formTitle} gutterBottom>
+				{moment(event.start).format('DD MMMM, YYYY')}
+			</Typography>
+			<form className={classes.form} noValidate>
 				{
-					event.id && 
-						<Button
-							variant="outlined"
-							color="primary"
-							className={classes.formButton}
-							onClick={handleDeleteBtnClick}
-						>
-							Delete
-						</Button>
+					serverError && 
+						<AlertComponent type='error' identifier='server' text='' />
 				}
-				<Button 
-					variant="outlined"
-					color='primary'
-					className={classes.formButton}
-					onClick={handleSaveBtnClick}
-				>
-					Save
-				</Button>
-				<Button
-					variant="outlined"
-					color="primary"
-					onClick={fnClose}
-					className={classes.formButton}
-				>
-					Cancel
-				</Button>
-			</footer>
-		</form>
+				<TextField
+					label="Date"
+					type="date"
+					defaultValue={moment(event.start).format('YYYY-MM-DD')}
+					className={classes.inputTextField}
+					InputProps={{
+						readOnly: true,
+						className: classes.input
+					}}
+					InputLabelProps={{
+						shrink: true,
+					}}
+				/>
+				
+				<Editor
+					toolbar={{options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'emoji',]	}}
+					editorState={editorState}
+					initialContentState={{}}
+					toolbarClassName="editor-toolbar"
+					wrapperClassName="editor-wrapper"
+					editorClassName="editor"
+					onEditorStateChange={(newState) => setEditorState(newState)}
+				/>
+				<footer className={classes.formFooter}>
+					{
+						event.id && 
+							<Button
+								variant="outlined"
+								color="primary"
+								className={classes.formButton}
+								onClick={handleDeleteBtnClick}
+							>
+								Delete
+							</Button>
+					}
+					<Button 
+						variant="outlined"
+						color='primary'
+						className={classes.formButton}
+						onClick={handleSaveBtnClick}
+					>
+						Save
+					</Button>
+					<Button
+						variant="outlined"
+						color="primary"
+						onClick={fnClose}
+						className={classes.formButton}
+					>
+						Cancel
+					</Button>
+				</footer>
+			</form>
+		</>
 	)
 }
