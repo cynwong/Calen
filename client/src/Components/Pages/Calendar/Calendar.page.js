abf5b79056bc1a0fc46237bfe6c6ede95634629f
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-27 21:02:33 +1100

    Update Calendar Page to reflect fullcalender chang

--- client/src/Components/Pages/Calendar/Calendar.page.js
+++ client/src/Components/Pages/Calendar/Calendar.page.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 
+import { useHistory } from 'react-router-dom';
+
 import { Container, Paper } from '@material-ui/core';
 
 import FullCalendarComponent from '../../common/FullCalendar/FullCalendar';
@@ -7,12 +9,23 @@ import FullCalendarComponent from '../../common/FullCalendar/FullCalendar';
 import AppContext from '../../../utils/AppContext'
 
 export default function DashBoard() {
-	const { classes } = useContext(AppContext);
+	const { classes, user: { events } } = useContext(AppContext);
+
+	const history = useHistory();
+
+	const selectDates = ({ allDay, endStr, startStr}) => {
+		history.push(`/events/new?start=${startStr}&end=${endStr}&allDay=${allDay}`);
+	};
+
+	const eventClick = ({event}) => {
+		const {id} = event;
+		history.push(`events/${id}`);
+	};
 
 	return (
 		<Container className={classes.container}>
 			<Paper className={classes.bigPaper}>
-				<FullCalendarComponent />
+				<FullCalendarComponent events={events} selectDates={selectDates} eventClick={eventClick} leftHeader='today' rightHeader='dayGridMonth,timeGridWeek,timeGridDay,listDay' />
 			</Paper>
 		</Container>
 	)
