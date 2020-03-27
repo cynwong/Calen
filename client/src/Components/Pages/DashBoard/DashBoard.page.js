2e58a1b4318ed11f7c9cda68f170c036c3d62e6a
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-27 14:21:51 +1100

    Change Dashboard wrapper to Container component

--- client/src/Components/Pages/DashBoard/DashBoard.page.js
+++ client/src/Components/Pages/DashBoard/DashBoard.page.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import {
+	Container
+} from '@material-ui/core';
+
 // Styles
 import './DashBoard.styles.scss';
 
 export default function DashBoard() {
 
 	return (
-		<div className='calendar-container'>
-			Hello...
-		</div>
+		<Container fluid>
+			Hello
+		</Container>
 	)
 }
