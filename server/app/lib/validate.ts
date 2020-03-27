b29a1351d9e16324f2abae5efefe4d3e8c8e5494
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-28 10:06:25 +1100

    eslint fixes

--- server/app/lib/validate.ts
+++ server/app/lib/validate.ts
@@ -1,5 +1,5 @@
 
-export const validateEmail = (email:string):string[]=> {
+export const validateEmail = (email:string):string[] => {
 	const errors = [];
 	if (!email) {
 		errors.push('Email is required');
@@ -8,24 +8,17 @@ export const validateEmail = (email:string):string[]=> {
 		errors.push('Email is not valid');
 	}
 	return errors;
-}
+};
 
-export const validatePassword = (password:string):string[]=> {
+export const validatePassword = (password:string):string[] => {
 	const errors = [];
 	if (!password) {
 		errors.push('Password is required');
 	}
 	const passwordLength = password.length;
 	if (passwordLength < 8 || passwordLength > 20) {
-		errors.push('Password must be 8-20 characters long.')
+		errors.push('Password must be 8-20 characters long.');
 	}
 
-	// if ( 
-	// 	new RegExp(/([A-Z])/g).test(password) &&
-	// 	new RegExp(/([a-z])/g).test(password) &&
-	// 	new RegExp(/([0-9])/g).test(password) &&
-
-	// )
-
 	return errors;
-}
+};
