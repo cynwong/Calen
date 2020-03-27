1a6f0c6b1a3859df876d3f1081fc39ff39bfe75e
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-27 17:14:48 +1100

    remove unused components

--- client/src/Components/common/CloseButton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './styles.scss';
-
-export default function CloseButton(props) {
-	return (
-		<button className="btnClose" onClick={props.onClick}>x</button>
-	)
-}
