6a576ddef8d1e0e6e01d6497a3ea4afd9fa0f9db
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-28 13:01:12 +1100

    Add isLogin Route

---
+++ server/app/routes/api/isLogIn/isLogIn.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+
+import { UserDocument } from '../../../../database/models/User';
+import { getAllEvents } from '../../../../database/controllers/EventController';
+
+export default async function isUserLoginAPI(req: Request, res: Response): Promise<void> {
+	const { user } = req;
+	if (!user) {
+		res.status(200).json({
+			success: true,
+			user: undefined,
+		});
+		return;
+	}
+	const {
+		id,
+		email,
+		firstName,
+		lastName,
+	} = user as UserDocument;
+	const events = await getAllEvents(id);
+
+	res.status(200).json({
+		success: true,
+		data: {
+			user: {
+				id,
+				username: email,
+				firstName,
+				lastName,
+				events,
+			},
+		},
+	});
+};
