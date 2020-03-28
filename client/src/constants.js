e58912dd4b561c13fe4008c905360890cb9546cd
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-28 19:31:44 +1100

    constant

---
+++ client/src/constants.js
@@ -0,0 +1,12 @@
+
+export const defaultSettings = {
+	calendarColour: '#b7a92f',
+	diaryColour: '#c91975',
+	taskColour: '#e45912',
+	mealPlanColour: '#20dea1', 
+	googleCalendarIds: []
+}
+
+export const formatDateString = 'YYYY-MM-DDTHH:mm';
+
+export const parseDateString = 'YYYY-MM-DDTHH:mm Z';
