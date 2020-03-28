9d6497c0d8659f9fb05c1d2749c231591540fcc9
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-28 19:35:41 +1100

    setting model

---
+++ server/database/models/Setting.ts
@@ -0,0 +1,52 @@
+import {
+	Document, model, Model, Schema,
+} from 'mongoose';
+
+export interface SettingDocument extends Document {
+	_id: string,
+	id: string,
+	userId: string,
+	calendarColour: string,
+	diaryColour: string,
+	taskColour: string,
+	mealPlanColour: boolean,
+	googleCalendarIds: string[],
+	createdAt: Date,
+	updatedAt: Date
+}
+const settingSchema: Schema<SettingDocument> = new Schema<SettingDocument>({
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: 'user',
+	},
+	calendarColour: {
+		type: String,
+		trim: true,
+	},
+	diaryColour: {
+		type: String,
+		trim: true,
+	},
+	taskColour: {
+		type: String,
+		trim: true,
+	},
+	mealPlanColour: {
+		type: String,
+		trim: true,
+	},
+	googleCalendarIds: {
+		type: [String],
+		trim: true,
+	},
+}, {
+	timestamps: true,
+	toObject: { virtuals: true },
+	toJSON: { virtuals: true },
+});
+
+settingSchema.virtual('id').get(function (this:SettingDocument) {
+	return this?._id;
+});
+
+export const Event: Model<SettingDocument> = model<SettingDocument>('event', settingSchema);
