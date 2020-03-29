5f09aef66698012a7c08a2344f95f54b52cf40a7
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-29 20:35:58 +1100

    indexDBConfig

---
+++ client/src/config/indexDBConfig.js
@@ -0,0 +1,14 @@
+export const DBConfig = {
+	name: 'calen',
+	version: 1,
+	objectStoresMeta: [
+		{
+			store: 'events',
+			storeConfig: { keyPath: 'id', autoIncrement: true },
+			storeSchema: [
+				{ name: 'action', keypath: 'action', options: { unique: false } },
+				{ name: 'data', keypath: 'data', options: { unique: false } }
+			]
+		}
+	]
+};
